d19aef2b618506a8f7d965a2615d92bc208b145f
Benjamin Koch <bbbsnowball@gmail.com>
2024-05-21 02:54:33 +0200

    add some of the new features in opencv.py to opencv_tk.py

--- opencv_tk.py
+++ opencv_tk.py
@@ -5,11 +5,37 @@ import math
 import ht301_hacklib
 import utils
 import time
+from skimage.exposure import rescale_intensity, equalize_hist
+import pickle
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
-draw_temp = False
+def increase_luminance_contrast(frame):
+    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+    l_channel, a, b = cv2.split(lab)
+
+    # Applying CLAHE to L-channel
+    # feel free to try different values for the limit and grid size:
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l_channel)
+
+    # merge the CLAHE enhanced L-channel with the a and b channel
+    limg = cv2.merge((cl, a, b))
+    frame = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    return frame
+
+def rotate_frame(frame, orientation):
+    if orientation == 0:
+        return frame
+    elif orientation == 90:
+        return np.rot90(frame).copy()
+    elif orientation == 180:
+        return np.rot90(frame, 2).copy()
+    elif orientation == 270:
+        return np.rot90(frame, 3).copy()
+    else:
+        return frame
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -35,6 +61,11 @@ class App(tk.Frame):
         #self.calibrate = -1
         self.calibrate = 0  # We start with calibration anyway.
         self.calibration_image = None
+        self.delay_to_calibrate = -1
+        self.orientation = 0  # 0, 90, 180, 270
+        self.draw_temp = False
+        self.upscale_factor = 2
+        self.verbose_info = True
 
     def show_frame(self):
         ret, frame = self.cap.read()
@@ -53,14 +84,28 @@ class App(tk.Frame):
             #print("%s - %s = %s" % (a, self.calibration_image[0][0], frame[0][0]))
         frame_min2 = frame.min()
         frame_max2 = frame.max()
-        if True:
+        if False:
             # Sketchy auto-exposure
             frame -= frame.min()
             frame /= max(1, frame.max())
-        frame = (np.clip(frame, 0, 1)*255).astype(np.uint8)
-        frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+            frame = (np.clip(frame, 0, 1)*255).astype(np.uint8)
+            frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+        elif True:
+            frame = rescale_intensity(
+                equalize_hist(frame), in_range="image", out_range=(0, 255)
+            ).astype(np.uint8)
 
-        if draw_temp:
+            frame = cv2.applyColorMap(frame, cv2.COLORMAP_INFERNO)
+
+        increase_luminance_contrast(frame)
+
+        frame = rotate_frame(frame, self.orientation)
+
+        frame = np.kron(frame, np.ones((self.upscale_factor, self.upscale_factor, 1))).astype(
+            np.uint8
+        )
+
+        if self.draw_temp:
             utils.drawTemperature(frame, info['Tmin_point'], info['Tmin_C'], (55,0,0))
             utils.drawTemperature(frame, info['Tmax_point'], info['Tmax_C'], (0,0,85))
             utils.drawTemperature(frame, info['Tcenter_point'], info['Tcenter_C'], (0,255,255))
@@ -69,7 +114,7 @@ class App(tk.Frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.cv_frame = frame
         frame = Image.fromarray(frame)
-        frame = frame.resize((frame.width*2, frame.height*2))
+        #frame = frame.resize((frame.width*2, frame.height*2))
         frame = ImageTk.PhotoImage(frame)
         self.imgLabel.image = frame
         self.imgLabel.configure(image=frame)
@@ -79,50 +124,63 @@ class App(tk.Frame):
         if self.calibrate >= 0:
             self.calibrate += 1
 
+        if self.delay_to_calibrate >= 0:
+            self.delay_to_calibrate -= 1
+            if self.delay_to_calibrate == 0:
+                self.cap.calibrate()
+                self.calibrate = 0
+
         s = ""
-        i = 0
-        per_line = 24
-        s += "     "
-        for j in range(0, per_line):
-            s += " %4d" % j
-        s += "\n"
-        meta = self.cap.frame_raw_u16[self.cap.fourLinePara:]
-        for j in range(0, len(meta), per_line):
-            s += "%04x:" % (2*i)
-            for k in range(j, min(j+per_line, len(meta))):
-                s += " %04x" % meta[k]
-                i += 1
-            if len(meta) < j+per_line:
-                for k in range(len(meta), j+per_line):
-                    s += "     "
-            s += " | "
-            for k in range(j, min(j+per_line, len(meta))):
-                a = meta[k] & 0xff
-                if chr(a) >= ' ':
-                    s += chr(a)
-                else:
-                    s += "."
-                a = (meta[k] >> 8) & 0xff
-                if chr(a) >= ' ':
-                    s += chr(a)
-                else:
-                    s += "."
+        if self.verbose_info:
+            i = 0
+            per_line = 24
+            s += "     "
+            for j in range(0, per_line):
+                s += " %4d" % j
             s += "\n"
-        s += "min: %f\nmax: %f\n" % (frame_min, frame_max)
-        s += "min: %f\nmax: %f\n" % (frame_min2, frame_max2)
-        s += "meta[0] = 0:%d, 1:%d, 16:%d\n" % (meta[0], meta[1], meta[16])
-        s += "meta[1] = 0:%d, 1:%d\n" % (meta[256], meta[257])
-        s += "\n".join(self.history[-3:])
+            meta = self.cap.frame_raw_u16[self.cap.fourLinePara:]
+            for j in range(0, len(meta), per_line):
+                s += "%04x:" % (2*i)
+                for k in range(j, min(j+per_line, len(meta))):
+                    s += " %04x" % meta[k]
+                    i += 1
+                if len(meta) < j+per_line:
+                    for k in range(len(meta), j+per_line):
+                        s += "     "
+                s += " | "
+                for k in range(j, min(j+per_line, len(meta))):
+                    a = meta[k] & 0xff
+                    if chr(a) >= ' ':
+                        s += chr(a)
+                    else:
+                        s += "."
+                    a = (meta[k] >> 8) & 0xff
+                    if chr(a) >= ' ':
+                        s += chr(a)
+                    else:
+                        s += "."
+                s += "\n"
+            s += "min: %f\nmax: %f\n" % (frame_min, frame_max)
+            s += "min: %f\nmax: %f\n" % (frame_min2, frame_max2)
+            s += "meta[0] = 0:%d, 1:%d, 16:%d\n" % (meta[0], meta[1], meta[16])
+            s += "meta[1] = 0:%d, 1:%d\n" % (meta[256], meta[257])
+        if self.delay_to_calibrate >= 0:
+            s += "waiting to calibrate... %d...\n" % self.delay_to_calibrate
+        if 0 <= self.calibrate <= 30:
+            s += "calibrating...\n"
+        if self.verbose_info:
+            s += "\n".join(self.history[-3:]) + "\n"
         self.metaLabel.text = s
         self.metaLabel.configure(text=s)
 
-        if self.prev_meta is not None:
+        if self.verbose_info and self.prev_meta is not None:
             for x in range(len(meta)):
                 if self.prev_meta[x] != meta[x] and x not in (0, 1):
                     msg = "meta[%d] changed from 0x%04x=%d to 0x%04x=%d" % (x, self.prev_meta[x], self.prev_meta[x], meta[x], meta[x])
                     print(msg)
                     self.history.append(msg)
-        self.prev_meta = meta
+        if self.verbose_info:
+            self.prev_meta = meta
 
         root.after(10, self.show_frame)
 
@@ -134,6 +192,33 @@ class App(tk.Frame):
             self.calibrate = 0
         elif e.char == 's' or e.char == 'w':
             cv2.imwrite(time.strftime("%Y-%m-%d_%H:%M:%S") + '.png', self.cv_frame)
+        elif e.char == 'k':
+            self.cap.temperature_range_normal()
+            # some delay is needed before calibration
+            self.delay_to_calibrate = 100 # 50
+            self.calibration_image = None
+        elif e.char == 'l':
+            self.cap.temperature_range_high()
+            # some delay is needed before calibration
+            self.delay_to_calibrate = 150 # 50
+            self.calibration_image = None
+        elif e.char == 'o':
+            self.orientation = (self.orientation - 90) % 360
+        elif e.char == 'a':
+            # save to disk
+            ret, frame = self.cap.read()
+            data = (frame)
+            name = time.strftime("%Y-%m-%d_%H-%M-%S") + ".pkl"
+            with open(name, "wb") as f:
+                pickle.dump(data, f)
+        elif e.char == 't':
+            self.draw_temp = not self.draw_temp
+        elif e.char == 'v':
+            self.verbose_info = not self.verbose_info
+            if self.verbose_info:
+                self.upscale_factor = 2
+            else:
+                self.upscale_factor = 4
     def keyup(self, e):
         pass
 
